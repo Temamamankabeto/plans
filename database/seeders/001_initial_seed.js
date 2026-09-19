e6ead663495b71bd43616d55a36525fd252ea4a8
Temamaman <temamaman156@gmail.com>
2026-09-19 16:36:25 +0300

    corrected maping

--- database/seeders/001_initial_seed.js
+++ database/seeders/001_initial_seed.js
@@ -205,5 +205,7 @@ module.exports = async function seed(connection) {
     await connection.execute(`DELETE FROM user_roles WHERE user_id = ?`, [userId]);
     await connection.execute(`INSERT IGNORE INTO user_roles (user_id, role_id) VALUES (?, ?)`, [userId, roleId]);
   }
+
+  
   
 };
